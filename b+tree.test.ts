1f7710b493b825f6cce837c6f1fd268dd893b370
Craig Macomber <Craig.Macomber@microsoft.com>
2022-03-03 11:08:20 -0800

    add mergeSibling regression test

--- b+tree.test.ts
+++ b+tree.test.ts
@@ -468,6 +468,62 @@ describe("cloning and sharing tests", () => {
     // do some private field access and any casts to make it work.
     expect((clone['_root'] as any).children[0].children[0]).not.toBe((tree['_root'] as any).children[0].children[0]);
   });
+
+  test("Regression test for mergeSibling setting isShared", () => {
+    // This tests make a 3 layer tree (height = 2), so use a small branching factor.
+    const maxNodeSize = 4;
+    const tree = new BTree<number, number>(
+      undefined,
+      simpleComparator,
+      maxNodeSize
+    );
+    // Build a 3 layer tree
+    const count =maxNodeSize * maxNodeSize * maxNodeSize;
+    for (
+      let index = 0;
+      index < count;
+      index++
+    ) {
+      tree.set(index, 0);
+    }
+    // Leaf nodes don't count, so this is depth 2
+    expect(tree.height).toBe(2);
+
+    // Delete most of the keys so merging interior nodes is possible, marking all nodes as shared.
+    for (
+      let index = 0;
+      index < maxNodeSize * maxNodeSize * maxNodeSize;
+      index++
+    ) {
+      if (index % 4 !== 0) {
+        tree.delete(index);
+      }
+    }
+
+    const deepClone = tree.greedyClone(true);
+    const cheapClone = tree.clone();
+
+    // These two clones should remain unchanged forever.
+    // The bug this is testing for resulted in the cheap clone getting modified:
+    // we will compare it against the deep clone to confirm it does not.
+
+
+    // Delete a bunch more nodes, causing merging.
+    for (
+      let index = 0;
+      index < maxNodeSize * maxNodeSize * maxNodeSize;
+      index++
+    ) {
+      if (index % 16 !== 0) {
+        tree.delete(index);
+      }
+    }
+
+    const different: number[] = [];
+    const onDiff = (k) => { different.push(k); }
+    deepClone.diffAgainst(cheapClone, onDiff, onDiff, onDiff);
+    expect(different).toEqual([]);
+  });
 });
 
 describe('B+ tree with fanout 32', testBTree.bind(null, 32));
