51e5ab57a7c35c5227545ab346eee76bf954e2ab
Taylor Williams <taylorsw@microsoft.com>
2025-11-11 15:54:57 -0800

    stub

--- extended/index.ts
+++ extended/index.ts
@@ -22,6 +22,18 @@ export class BTreeEx<K = any, V = any> extends BTree<K, V> {
     return result as this;
   }
 
+  /**
+   * Computes the differences between `this` and `other`.
+   * For efficiency, the diff is returned via invocations of supplied handlers.
+   * The computation is optimized for the case in which the two trees have large amounts of shared data
+   * (obtained by calling the `clone` or `with` APIs) and will avoid any iteration of shared state.
+   * The handlers can cause computation to early exit by returning `{ break: R }`.
+   * Neither collection should be mutated during the comparison (inside your callbacks), as this method assumes they remain stable.
+   * @param other The tree to compute a diff against.
+   * @param onlyThis Callback invoked for all keys only present in `this`.
+   * @param onlyOther Callback invoked for all keys only present in `other`.
+   * @param different Callback invoked for all keys with differing values.
+   */
   diffAgainst<R>(
     other: BTree<K, V>,
     onlyThis?: (k: K, v: V) => { break?: R } | void,
@@ -29,6 +41,42 @@ export class BTreeEx<K = any, V = any> extends BTree<K, V> {
     different?: (k: K, vThis: V, vOther: V) => { break?: R } | void
   ): R | undefined {
     return diffAgainstAlgorithm(this, other, onlyThis, onlyOther, different);
+  }
+
+  /**
+   * Intersects this tree with `other`, calling the supplied `intersection` callback for each intersecting key/value pair.
+   * Neither tree is modified.
+   * @param other The other tree to intersect with this one.
+   * @param intersection Called for keys that appear in both trees.
+   * @description Complexity is bounded O(N + M) time and O(log(N + M)) for allocations.
+   * However, time is additionally bounded by O(log(N + M) * D) where D is the number of disjoint ranges of keys between
+   * the two trees. In practice, that means for keys of random distribution the performance is O(N + M) and for
+   * keys with significant numbers of non-overlapping key ranges it is O(log(N + M) * D) which is much faster.
+   * The algorithm achieves this additional non-linear bound by skipping over non-intersecting subtrees entirely.
+   * Note that in benchmarks even the worst case (fully interleaved keys) performance is faster than calling `toArray`
+   * on both trees and performing a walk on the sorted contents due to the reduced allocation overhead.
+   */
+  intersect(other: BTree<K,V>, intersection: (key: K, leftValue: V, rightValue: V) => void): void {
+
+  }
+
+  /**
+   * Efficiently merges this tree with `other`, reusing subtrees wherever possible.
+   * Neither input tree is modified.
+   * @param other The other tree to merge into this one.
+   * @param merge Called for keys that appear in both trees. Return the desired value, or
+   *        `undefined` to omit the key from the result.
+   * @returns A new BTree that contains the merged key/value pairs.
+   * @description Complexity is bounded O(N + M) for both time and allocations.
+   * However, it is additionally bounded by O(log(N + M) * D) where D is the number of disjoint ranges of keys between
+   * the two trees. In practice, that means for keys of random distribution the performance is O(N + M) and for
+   * keys with significant numbers of non-overlapping key ranges it is O(log(N + M) * D) which is much faster.
+   * The algorithm achieves this additional non-linear bound by skipping over non-intersecting subtrees entirely.
+   * Note that in benchmarks even the worst case (fully interleaved keys) performance is faster than cloning `this`
+   * and inserting the contents of `other` into the clone.
+   */
+  merge(other: BTree<K,V>, merge: (key: K, leftValue: V, rightValue: V) => V | undefined): BTreeEx<K,V> {
+
   }
 }
 
