4262b1c412ef4342120c324439bcdec0d6ca143b
Taylor Williams <taylorsw@microsoft.com>
2025-11-13 19:02:39 -0800

    docs

--- extended/union.ts
+++ extended/union.ts
@@ -8,7 +8,7 @@ import { decompose, buildFromDecomposition } from "./decompose";
  * @param treeA First tree to union.
  * @param treeB Second tree to union.
  * @param combineFn Called for keys that appear in both trees. Return the desired value, or
- *        `undefined` to omit the key from the result.
+ *        `undefined` to omit the key from the result. Note: symmetric difference can be achieved by always returning `undefined`.
  * @returns A new BTree that contains the unioned key/value pairs.
  * @description Complexity is bounded O(N + M) for both time and allocations.
  * However, it is additionally bounded by O(log(N + M) * D) where D is the number of disjoint ranges of keys between
